2143819aee81e5042e1ea4b8858ac888cc6b46d0
jinwoo827 <pleasure1988827@gmail.com>
2017-07-14 10:34:33 +0900

    test2

--- shop/src/member/Service.java
+++ shop/src/member/Service.java
@@ -1,5 +1,10 @@
 package member;
 
 public interface Service {
+	void join(Member m);
+	Member getMember(String id);
+	void editMember(Member m);
+	void remMember(String id);
+	boolean login(String id, String pwd);
 
 }
--- shop/src/member/ServiceImpl.java
+++ shop/src/member/ServiceImpl.java
@@ -1,5 +1,45 @@
 package member;
 
-public class ServiceImpl {
+public class ServiceImpl implements Service{
+	private Dao dao;
+	public ServiceImpl(Dao dao) {
+		this.dao = dao;
+	}
+
+	@Override
+	public void join(Member m) {
+		// TODO Auto-generated method stub
+		dao.insert(m);
+	}
+
+	@Override
+	public Member getMember(String id) {
+		// TODO Auto-generated method stub
+		return dao.select(id);
+	}
+
+	@Override
+	public void editMember(Member m) {
+		// TODO Auto-generated method stub
+		dao.update(m);
+	}
+
+	@Override
+	public void remMember(String id) {
+		// TODO Auto-generated method stub
+		dao.delete(id);
+	}
+
+	@Override
+	public boolean login(String id, String pwd) {
+		// TODO Auto-generated method stub
+		boolean flag = false;
+		Member m = getMember(id);
+		if(m != null && pwd.equals(m.getPwd())) {
+			flag = true;
+		}
+		
+		return flag;
+	}
 
 }
